957bedd6d569e7a550c76c4d91011bf8034bf3fc
Fox Hsu @ M4800 in 2022 <shenfa@shenfa.idv.tw>
2022-08-13 19:18:59 +0800

    Reading potentio meter works.

--- Square_Shifter/Square_Shifter.ino
+++ Square_Shifter/Square_Shifter.ino
@@ -10,14 +10,17 @@
 //---------------------------------------------------------------------------
 //---------------------------------------------------------------------------
 
-#define		Pin_DO		(4)
-#define		Pin_DI		(3)
+#define		Pin_DO			(13)
+#define		Pin_DI			(2)
 
-#define		Out_Level_High	(0)
-#define		Out_Level_Low	(1)
+#define		Pin_Potentiometer	(A0)
+#define		Potentiometer_Const	(856)
 
-#define		In_Level_High	(0)
-#define		In_Level_Low	(1)
+#define		Out_Level_High		(0)
+#define		Out_Level_Low		(1)
+
+#define		In_Level_High		(1)
+#define		In_Level_Low		(0)
 
 
 void	setup()
@@ -31,7 +34,7 @@ void	setup()
 
 	digitalWrite( Pin_DO, Out_Level_Low );
 
-	Serial.println("MIPI Host Initialized!");
+	Serial.println("Initialized!");
 }
 
 //---------------------------------------------------------------------------
@@ -39,11 +42,22 @@ void	setup()
 
 
 
+int	Read_Potentiometer( void )
+{
+	int	val;
 
+	val = analogRead( Pin_Potentiometer );
+	return( Potentiometer_Const - val );
+}
 
 void	loop()
 {
 	// put your main code here, to run repeatedly:
+
+	int	val;
+
+	val = Read_Potentiometer();
+	Serial.println( val );		// debug value
 
 	delay( 1000 );
 }
