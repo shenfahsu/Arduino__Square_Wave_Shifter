9ba74641fe9f2aeb50336ccc0502e941ccfa265c
Fox Hsu @ M4800 in 2022 <shenfa@shenfa.idv.tw>
2022-08-14 20:48:32 +0800

    Major function, shift phase, works.

--- Square_Shifter/Square_Shifter.ino
+++ Square_Shifter/Square_Shifter.ino
@@ -10,26 +10,48 @@
 //---------------------------------------------------------------------------
 //---------------------------------------------------------------------------
 
-#define		Pin_DO			(13)
-#define		Pin_DI			(2)
+#define		Pin_DO				(7)
+#define		Pin_DI				(2)
 
-#define		Pin_Potentiometer	(A0)
-#define		Potentiometer_Const	(856)
+#define		Pin_Potentiometer		(A0)
+#define		Potentiometer_Const		(856)
+#define		Potentiometer_Phase_Step	(22.22)
 
-#define		Out_Level_High		(0)
-#define		Out_Level_Low		(1)
+#define		Out_Level_High			(0)
+#define		Out_Level_Low			(1)
 
-#define		In_Level_High		(1)
-#define		In_Level_Low		(0)
+#define		In_Level_High			(1)
+#define		In_Level_Low			(0)
+
+enum	Level_State__Enum {
+	Level_State__Triggered,
+	Level_State__Low,
+	Level_State__High,
+
+	Level_State__Enums
+};
+
+
+enum	Level_State__Enum	Current_State = Level_State__Enums;
+unsigned int			Phase_Delay_uS = 0;
+int				Potentiometer__Last = 0;
+
+void	EInt_ISR()
+{
+	Current_State = Level_State__Triggered;
+}
 
 
 void	setup()
 {
+	// put your setup code here, to run once:
 	Serial.begin( 9600 );
 
-	// put your setup code here, to run once:
+	pinMode( Pin_DI, INPUT_PULLUP );
+	attachInterrupt( digitalPinToInterrupt( Pin_DI ), EInt_ISR, FALLING );
+
 //	pinMode( Pin_DI, INPUT_PULLUP );
-	pinMode( Pin_DI, INPUT );
+// 	pinMode( Pin_DI, INPUT );
 	pinMode( Pin_DO, OUTPUT );
 
 	digitalWrite( Pin_DO, Out_Level_Low );
@@ -41,25 +63,52 @@ void	setup()
 //---------------------------------------------------------------------------
 
 
-
 int	Read_Potentiometer( void )
 {
 	int	val;
 
 	val = analogRead( Pin_Potentiometer );
+	if( Potentiometer__Last != val ) {
+		Potentiometer__Last = val;
+		Serial.println( val );		// debug value
+	}
 	return( Potentiometer_Const - val );
+}
+
+int	Get_Phase_Delay( void )
+{
+	int	val;
+	float	phase_step = Potentiometer_Phase_Step;
+
+	val = Read_Potentiometer();
+	if( val > 720 ) val = 720;
+	val >>= 1;
+	phase_step *= val;
+	return( phase_step / 2 );
 }
 
 void	loop()
 {
 	// put your main code here, to run repeatedly:
+	switch( Current_State ) {
+	case Level_State__Triggered:
+		Phase_Delay_uS = Get_Phase_Delay();
+		Current_State = Level_State__Low;
+		delayMicroseconds( Phase_Delay_uS );
 
-	int	val;
+	case Level_State__Low:
+		digitalWrite( Pin_DO, Out_Level_Low );
+		Current_State = Level_State__High;
+		delayMicroseconds( 4000 );
 
-	val = Read_Potentiometer();
-	Serial.println( val );		// debug value
+	case Level_State__High:
+		digitalWrite( Pin_DO, Out_Level_High );
+// 		Phase_Delay_uS = Get_Phase_Delay();
+		Current_State = Level_State__Enums;
 
-	delay( 1000 );
+	default:
+		break;
+	}
 }
 
 //---------------------------------------------------------------------------
